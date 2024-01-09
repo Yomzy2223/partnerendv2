15025346612b1f032934f28d82f2148ab9334063
yomzy2223 <oluwolesayo10@gmail.com>
2024-01-09 17:56:03 +0100

    Adjusted the spacing

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -16,58 +16,58 @@ const Home = () => {
   const { tableHeaders, tableBody } = useTableInfo();
 
   return (
-    <DoChecks items={["d"]} emptyText="You have not added any product">
-      <div className="flex flex-col gap-8 pt-4 pb-6 lg:flex-row ">
-        <div className="flex flex-nowrap gap-8 flex-1 overflow-auto px-1 py-2 lg:w-1/2 lg:grid lg:grid-cols-2">
-          <ServiceSummaryCard
-            title="Samuel Olajide"
-            info="Joined Sidebrief on the 13th of February 2023."
-          />
-          <AnalyticsCard3
-            title="Completed requests"
-            total="0"
-            current={244}
-            previous={87}
-          />
-          <AnalyticsCard3
-            title="Completed requests"
-            total="0"
-            current={24}
-            previous={87}
-          />
-          <AnalyticsCard3
-            title="Completed requests"
-            total="0"
-            current={244}
-            previous={87}
-          />
-        </div>
-        <CardWrapper
-          title="Newly added tasks"
-          className="gap-6 justify-between my-2 flex-1 p-0"
-        >
-          hello
-        </CardWrapper>
+    <DoChecks
+      items={["d"]}
+      emptyText="You have not added any product"
+      className="grid grid-cols-2 gap-8"
+    >
+      <div className="flex flex-nowrap gap-8 flex-1 overflow-auto px-1 py-2 lg:grid lg:grid-cols-2">
+        <ServiceSummaryCard
+          title="Samuel Olajide"
+          info="Joined Sidebrief on the 13th of February 2023."
+        />
+        <AnalyticsCard3
+          title="Completed requests"
+          total="0"
+          current={244}
+          previous={87}
+        />
+        <AnalyticsCard3
+          title="Completed requests"
+          total="0"
+          current={24}
+          previous={87}
+        />
+        <AnalyticsCard3
+          title="Completed requests"
+          total="0"
+          current={244}
+          previous={87}
+        />
       </div>
+      <CardWrapper
+        title="Newly added tasks"
+        className="gap-6 justify-between my-2 flex-1 p-0"
+      >
+        hello
+      </CardWrapper>
 
-      <div className="flex flex-col gap-6 lg:flex-row lg:gap-8">
-        <CardWrapper className="p-0" title="Ongoing tasks">
-          <GeneralTable
-            tableHeaders={tableHeaders}
-            tableBody={tableBody}
-            hideSearch
-            // serviceTableNav={serviceTableNav}
-          />
-        </CardWrapper>
-        <CardWrapper>
-          <AnalyticsHeader
-            title="Payment analytics"
-            description="Total revenue for registrations"
-            queryNav={paymentQueryNav}
-          />
-          <Image src={PaymentAnalyticsImg} alt="payment analytics" />
-        </CardWrapper>
-      </div>
+      <CardWrapper className="p-0" title="Ongoing tasks">
+        <GeneralTable
+          tableHeaders={tableHeaders}
+          tableBody={tableBody}
+          hideSearch
+          // serviceTableNav={serviceTableNav}
+        />
+      </CardWrapper>
+      <CardWrapper>
+        <AnalyticsHeader
+          title="Payment analytics"
+          description="Total revenue for registrations"
+          queryNav={paymentQueryNav}
+        />
+        <Image src={PaymentAnalyticsImg} alt="payment analytics" />
+      </CardWrapper>
     </DoChecks>
   );
 };
