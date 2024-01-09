f0e117dcd6aee6754ca2ef2d853a9feb9a1bbc59
yomzy2223 <oluwolesayo10@gmail.com>
2024-01-09 11:11:47 +0100

    Basic setup done

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;
+
+const serviceTableNav = [
+  {
+    name: "service",
+    value: "onboard",
+  },
+  {
+    name: "service",
+    value: "launch",
+  },
+  {
+    name: "service",
+    value: "manage",
+  },
+  {
+    name: "service",
+    value: "tax",
+  },
+];
