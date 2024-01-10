55d0a311877525b105bce82ca4524dce6d38ed52
Jesuloba <jesulobajohn@gmail.com>
2024-01-10 06:07:48 +0100

    ongoing

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -2,10 +2,12 @@ import ServiceAnalyticsImg from "./service-analytics.svg";
 import PaymentAnalyticsImg from "./payment-analytics.svg";
 import PaymentAnalyticsImg2 from "./payment-analytics2.svg";
 import EmptyContentSvg from "./empty-content.svg";
+import SearchIcon from "./search";
 
 export {
-  ServiceAnalyticsImg,
-  PaymentAnalyticsImg,
-  PaymentAnalyticsImg2,
-  EmptyContentSvg,
+	ServiceAnalyticsImg,
+	PaymentAnalyticsImg,
+	PaymentAnalyticsImg2,
+	EmptyContentSvg,
+	SearchIcon,
 };
