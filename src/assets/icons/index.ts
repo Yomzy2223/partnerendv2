3bad43aa1d133a4dc7ca7aa68382e4ab5f8b69bd
yomzy2223 <oluwolesayo10@gmail.com>
2024-02-02 08:55:32 +0100

    Auth now set up

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import TestimonialUser from "./testimonial_user.svg";
+
+export { Thumbs } from "./thumbs";
+export { Profilecard } from "./profilecard";
+export { FileShield } from "./fileshield";
+export { TestimonialUser };
