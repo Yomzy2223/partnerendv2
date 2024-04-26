3e631b0f9f6609bb60dd85216363b10b608752ca
Damola Olutoke <olutokeezekiel18@gmail.com>
2024-04-26 11:57:45 +0100

    edit profile changes

--- src/components/profile/EditProfileModal.tsx
+++ src/components/profile/EditProfileModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, Dispatch, ReactNode, SetStateAction } from "react";
+import React, { useState, Dispatch, ReactNode, useEffect } from "react";
 import { Modal, Button } from "@/components/flowbite";
 import Profile from './profile';
 import  ProfileForm  from "./ProfileForm"; 
@@ -6,6 +6,7 @@ import { ProfileSchema } from "./constants"
 import { Puff } from "react-loading-icons";
 import { FileInput } from "../file/fileInput";
 import useCountryApi from "@/hooks/useCountryApi";
+import { CountryTypes } from "@/types/type";
 
 export const EditProfileModal = ({
     title,
@@ -24,10 +25,33 @@ export const EditProfileModal = ({
 
     const [currentStep, setCurrentStep] = useState(1);
     const [loading, setLoading] = useState(false);
-
+    const [countriesData, setCountries] = useState<string[]>([]);
     const { getAllCountriesQuery } = useCountryApi();
     const { data: countries } = getAllCountriesQuery;
-    console.log("countries", countries?.data.data)
+
+    const countryList = countries?.data.data
+    console.log("countryList", countries?.data.data)
+
+    const countryNames: string[] = countryList?.map((country: CountryTypes) => {
+        const name = country.name;
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }) || [];
+    
+    console.log("countryNames", countryNames);
+
+    
+    useEffect(() => {
+        const fetchCountries = async () => {
+            try {
+                const response = await countries;
+                setCountries(response?.data?.data || []);
+                console.log("responses countries", response)
+            } catch (error) {
+                console.error("Error fetching countries:", error);
+            }
+        };
+        fetchCountries();
+    }, []);
 
     const handleNext = async () => {
         setLoading(true);
@@ -48,6 +72,61 @@ export const EditProfileModal = ({
         handleNext()
     };
 
+    const formInfo = [
+        {
+          name: "location",
+          label: "Where do you practice?",
+          type: "select",
+          selectOptions: countryNames,
+          textInputProp: {
+            placeholder: "Select Country",
+          },
+        }, 
+      
+        {
+          name:"name",
+          label:"Full Name",
+          type:"text",
+          textInputProp: {
+            placeholder: "Enter your full name",
+          },
+      
+        }, 
+      
+        {
+          name:"email",
+          label:"Email Address",
+          type:"email",
+          textInputProp: {
+            placeholder: "Enter your email address",
+          },
+        },
+      
+        {
+          name:"phoneNumber",
+          label:"Phone Number",
+          type:"text",
+          textInputProp: {
+            placeholder: "Enter your phone number",
+          },
+        },
+      
+        {
+          name:"payRange",
+          label:"Pay range",
+          type:"text",
+          textInputProp: {
+            placeholder: "Enter your pay range",
+          },
+        },
+    
+        {
+            name:"certificate",
+            label:"Certificate",
+            type:"file"
+        
+        },
+      ]
     const renderModalContent = () => {
         switch (currentStep) {
             case 1:
@@ -132,67 +211,7 @@ const defaultValues = {
   };
   
 
-  const formInfo = [
-    {
-      name: "location",
-      label: "Where do you practice?",
-      type: "select",
-      selectOptions: [
-        "Kenya",
-        "Nigeria",
-        "Ghana",
-        "South Africa",
-        "Tanzania",
-      ],
-      textInputProp: {
-        placeholder: "Select Country",
-      },
-    }, 
   
-    {
-      name:"name",
-      label:"Full Name",
-      type:"text",
-      textInputProp: {
-        placeholder: "Enter your full name",
-      },
-  
-    }, 
-  
-    {
-      name:"email",
-      label:"Email Address",
-      type:"email",
-      textInputProp: {
-        placeholder: "Enter your email address",
-      },
-    },
-  
-    {
-      name:"phoneNumber",
-      label:"Phone Number",
-      type:"text",
-      textInputProp: {
-        placeholder: "Enter your phone number",
-      },
-    },
-  
-    {
-      name:"payRange",
-      label:"Pay range",
-      type:"text",
-      textInputProp: {
-        placeholder: "Enter your pay range",
-      },
-    },
-
-    {
-        name:"certificate",
-        label:"Certificate",
-        type:"file"
-    
-    },
-  ]
 
   const documentsSubmitted = [
     {
