ab2598543d509484d928e47e0a2380264815f28e
yomzy2223 <oluwolesayo10@gmail.com>
2024-05-10 08:40:41 +0100

    Person card done

--- src/components/cards/personsCard/index.tsx
+++ src/components/cards/personsCard/index.tsx
@@ -8,25 +8,10 @@ import { Button, Card, Tabs } from "flowbite-react";
 import { Minus, Plus, User } from "lucide-react";
 import React, { useState } from "react";
 
-const PersonsCard = ({
-  title,
-  info,
-}: {
-  title: string;
-  info: {
-    field: string;
-    value: string;
-    type?: TFieldTypes;
-    fileName?: string;
-    fileLink?: string;
-    fileType?: string;
-    fileSize?: string;
-  }[][];
-}) => {
+const PersonsCard = ({ title, info }: { title: string; info: IPersonCard[][] }) => {
   const [clicked, setClicked] = useState<boolean>(false);
   const [position, setPosition] = useState(0);
 
-  const details = !clicked ? [info[position][0]] : info[position];
   const conditionalInfo = clicked ? [info[0]] : info;
 
   const showDetails = () => {
@@ -46,83 +31,97 @@ const PersonsCard = ({
         "border border-border m-4": !clicked,
       })}
     >
-      {conditionalInfo?.map((ind, i) => (
-        <div
-          // key={i}
-          className={cn("transition-all border border-border rounded space-y-4", {
-            "w-[235px]": !clicked,
-            "w-[600px] overflow-auto border-none": !!clicked,
-          })}
-        >
-          {/* </Navbar> */}
+      {conditionalInfo?.map((el, ind) => {
+        const details = !clicked ? [info[ind][0]] : info[position];
+
+        return (
           <div
-            className={cn("sticky left-0 flex justify-between gap-6 p-4 pb-0", {
-              "bg-background": clicked,
+            // key={i}
+            className={cn("transition-all border border-border rounded", {
+              "w-[235px]": !clicked,
+              "w-[600px] overflow-auto border-none": !!clicked,
             })}
           >
-            <div className="flex gap-4">
-              {Array(headers)
-                .fill("")
-                ?.map((ind, i) => (
-                  <Button
-                    color="ghost"
-                    size="fit"
-                    className={cn("text-foreground-5 px-2.5 py-0.5", {
-                      "bg-success text-success-foreground": position === i,
-                    })}
-                    onClick={() => setPosition(i)}
-                  >
-                    {title.toLowerCase() + " " + (i + 1)}
-                  </Button>
-                ))}
-            </div>
+            {/* </Navbar> */}
+            <div
+              className={cn("sticky left-0 flex justify-between gap-6 p-4 pb-0 bg-[#F9FAFB]", {
+                "bg-background pb-4": clicked,
+              })}
+            >
+              <div className="flex gap-4">
+                {Array(headers)
+                  .fill("")
+                  ?.map((el, i) => (
+                    <Button
+                      color="ghost"
+                      size="fit"
+                      className={cn("text-foreground-5 px-2.5 py-0.5", {
+                        "bg-success text-success-foreground": position === i,
+                      })}
+                      onClick={() => setPosition(i)}
+                    >
+                      {title.toLowerCase() + " " + ((clicked ? i : ind) + 1)}
+                    </Button>
+                  ))}
+              </div>
 
-            <Button className="cursor-pointer" size="fit" color="ghost">
-              {!clicked ? (
-                <Plus size={16} onClick={showDetails} />
-              ) : (
-                <Minus size={16} onClick={hideDetails} />
-              )}
-            </Button>
-          </div>
-          <div className="min-w-max bg-[#F9FAFB] p-4">
-            <div className="flex gap-4">
-              {!!clicked && <User />}
-              <div
-                className={cn("grid grid-cols-3 gap-4", {
-                  flex: !clicked,
-                })}
-              >
-                {details?.map((el) => (
-                  <div key={el?.field}>
-                    {el?.type === "document template" || el?.type === "document upload" ? (
-                      el.fileName && el.fileLink && el.fileSize && el.fileType ? (
-                        <FileInput
-                          fileName={el.fileName}
-                          fileLink={el.fileLink}
-                          fileSize={el.fileSize}
-                          fileType={el.fileType}
-                        />
+              <Button className="cursor-pointer" size="fit" color="ghost">
+                {!clicked ? (
+                  <Plus size={16} onClick={showDetails} />
+                ) : (
+                  <Minus size={16} onClick={hideDetails} />
+                )}
+              </Button>
+            </div>
+            <div className="min-w-max bg-[#F9FAFB] p-4">
+              <div className="flex gap-4">
+                {!!clicked && <User />}
+                <div
+                  className={cn("grid grid-cols-3 gap-4", {
+                    flex: !clicked,
+                  })}
+                >
+                  {details?.map((el) => (
+                    <div key={el?.field}>
+                      {el?.type === "document template" || el?.type === "document upload" ? (
+                        el.fileName && el.fileLink && el.fileSize && el.fileType ? (
+                          <FileInput
+                            fileName={el.fileName}
+                            fileLink={el.fileLink}
+                            fileSize={el.fileSize}
+                            fileType={el.fileType}
+                          />
+                        ) : (
+                          <div>
+                            <p className="text-base font-semibold text-foreground-9">{el.field}</p>
+                            <p className="text-foreground-5 text-sm italic">
+                              Document not uploaded yet
+                            </p>
+                          </div>
+                        )
                       ) : (
-                        <div>
-                          <p className="text-base font-semibold text-foreground-9">{el.field}</p>
-                          <p className="text-foreground-5 text-sm italic">
-                            Document not uploaded yet
-                          </p>
-                        </div>
-                      )
-                    ) : (
-                      <TextWithDetails title={el?.field} text={el?.value} />
-                    )}
-                  </div>
-                ))}
+                        <TextWithDetails title={el?.field} text={el?.value} />
+                      )}
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
 
 export default PersonsCard;
+
+export interface IPersonCard {
+  field: string;
+  value: string;
+  type?: TFieldTypes;
+  fileName?: string;
+  fileLink?: string;
+  fileType?: string;
+  fileSize?: string;
+}
